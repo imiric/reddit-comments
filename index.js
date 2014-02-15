66f96ccfe3f9f1b8285362793d7f46b94b57fbcc
Ivan Miric <imiric@gmail.com>
2014-02-15 16:29:12 +0100

    Added main object and initialization API

--- index.js
+++ index.js
@@ -2,9 +2,27 @@
 /**
  * Module dependencies.
  */
-
 var Emitter = require('emitter')
   , domify = require('domify')
   , events = require('event')
   , classes = require('classes')
   , query = require('query');
+
+exports = module.exports = RedditComments;
+
+function RedditComments(frame, options) {
+    options = options || {};
+    this.template = require('./template.html');
+    this.el = domify(this.template);
+    this.frame = query(frame);
+    this.options = options;
+};
+
+RedditComments.prototype.init = function() {
+    var self = this
+      , el = self.el
+      , frame = self.frame;
+
+    frame.innerHTML = self.template;
+    return self;
+};
